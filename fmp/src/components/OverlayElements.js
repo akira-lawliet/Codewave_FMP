f5e7dddf912de03bc8d8b2da19342a1cc9735934
kripesh101 <33386091+kripesh101@users.noreply.github.com>
2024-09-18 23:57:57 +0545

    adjust reference photo position

--- fmp/src/components/OverlayElements.js
+++ fmp/src/components/OverlayElements.js
@@ -3,7 +3,7 @@ import RefPhoto from "./RefPhoto";
 export default function OverlayElements() {
   return (
     <>
-      <div className="absolute w-[300px] h-full top-0 -left-0 items-center">
+      <div className="absolute w-full h-full top-0 left-0 items-center justify-end flex flex-row">
         <RefPhoto />
       </div>
     </>
