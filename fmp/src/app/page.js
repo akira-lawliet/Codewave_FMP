178594ea66dfb00ef609d7cbcf9ed73ac2d3ff44
aayush125 <aayushbhatt54@gmail.com>
2024-09-18 20:35:04 +0545

    Camera working

--- fmp/src/app/page.js
+++ fmp/src/app/page.js
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import VideoFeed from "@/components/VideoFeed";
-
 export default function Home() {
-  return <VideoFeed />;
+  return <>Home.</>;
 }
--- fmp/src/components/VideoFeed.js
+++ fmp/src/components/VideoFeed.js
@@ -1,3 +1,46 @@
-export default function VideoFeed() {
-  return <>This is video feed.</>;
-}
+"use client";
+
+import React, { useRef, useEffect } from "react";
+
+const VideoFeed = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    async function setupWebcam() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: {
+            facingMode: "environment",
+            width: { ideal: 1280 },
+            height: { ideal: 720 },
+          },
+        });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error("Error accessing webcam:", error);
+      }
+    }
+
+    setupWebcam();
+
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = videoRef.current.srcObject.getTracks();
+        tracks.forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
+  return (
+    <video
+      ref={videoRef}
+      autoPlay
+      playsInline
+      className="w-full h-full object-cover -scale-x-100"
+    />
+  );
+};
+
+export default VideoFeed;
