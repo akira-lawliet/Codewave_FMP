af8732d0fbcf92722ab756aa0034c86525d486f7
Azhe0 <82790572+Azhe0@users.noreply.github.com>
2024-09-18 20:39:52 +0545

    cm

--- fmp/src/app/page.js
+++ fmp/src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import VideoFeed from "@/components/VideoFeed";
-
+import Landing from "../components/Landing/page";
 export default function Home() {
-  return <VideoFeed />;
+  return <Landing />; 
 }
---
+++ fmp/src/components/Landing/page.js
@@ -0,0 +1,8 @@
+export default function Landing() {
+  return <>
+    <div className="hero bg-base-200 min-h-screen"> 
+      <div></div>
+
+    </div>
+  </>;
+}
